81311f894ce8af8a25236df17d1c84c2431ab265
Timofey Deryabin <timofeyderyabin86@gmail.com>
2022-12-10 13:17:30 +0500

    File1 changed by user1

--- file1.java
+++ file1.java
@@ -2,5 +2,6 @@ public class MyFirstApp {
     public static void main(String[] args) {
         System.out.println("Hello world!!!12345");
         System.out.println("Good bye!");
+        System.out.println("Line from user1");
     }
 }
